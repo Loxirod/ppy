0ca62b08cc4a85cc8e32149c05a5f5754d0dcce7
Tomasz Małkiński <s24234@pjwstk.edu.pl>
2023-03-07 09:41:02 +0100

    yo

--- new 1.py
+++ new 1.py
@@ -1,9 +1,9 @@
 import webbrowser
 url = input("podaj strone ")
 webbrowser.open(url)
-// git clone https://github.com/Loxirod/ppy.git
-// git add .
-// git commit
-//git push
-//git commit -m "add script for open web page"
-//git status
+# git clone https://github.com/Loxirod/ppy.git
+# git add .
+# git commit
+#git push
+#git commit -m "add script for open web page"
+#git status
